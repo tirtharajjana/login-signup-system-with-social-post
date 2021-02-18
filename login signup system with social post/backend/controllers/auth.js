89f026057d39021d7c79d4c4abc203d413de8f15
Tirtharaj <tirtharajjana615@gmail.com>
2021-02-18 23:32:36 +0530

    day2:last

--- login signup system with social post/backend/controllers/auth.js
+++ login signup system with social post/backend/controllers/auth.js
@@ -1,1 +1,7 @@
-const {validation } =require('express-validator');
+const { validation, validationResult } = require('express-validator');
+
+express.signup = (async (req, res, next) => {
+    const errors=validationResult(req);
+
+    if()
+})
